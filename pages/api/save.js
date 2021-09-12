4ecbf031624d3403c892cac3a5c43218e6a49fd8
João Macieira <jp.macieira@uol.com.br>
2021-09-12 09:34:41 -0300

    Corrigindo private key

--- pages/api/save.js
+++ pages/api/save.js
@@ -9,12 +9,17 @@ const genCupom = () => {
   return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
 }
 
+const fromBase64 = value => {
+  const buff = Buffer.from(value, 'base64')
+  return buff.toString('ascii')
+}
+
 const api = async (request, response) => {
   try {
     // Inicia a conexão com a planilha
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     });
     await doc.loadInfo();
 
